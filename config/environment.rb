208baaf3b6f6245a4523ab46d27d51e9f06456ea
brookton <davidbrookton@gmail.com>
2019-05-07 16:45:43 -0600

    Rake & environment setup

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,10 @@
+require_relative '../lib/plants.rb'
+require_relative '../db/plants.db'
+require 'require_all'
+require 'pry'
 require 'bundler'
+require 'sinatra/activerecord'
+
 Bundler.require
 
-ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
-require_all 'lib'
+ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: '../db/plants.db')
